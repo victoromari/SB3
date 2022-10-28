f8d44ff5d841aa2c44abdb7cf89297ea06c8915a
Victor <vgomari21@students.desu.edu>
2022-10-28 16:19:37 -0400

    new code

--- src/main/java/Grok.java
+++ src/main/java/Grok.java
@@ -1,3 +1,5 @@
+import javax.crypto.Mac;
+
 /**
  * Groks are bad actors in a game.  Groks have the ability to ingest
  * a PowerPill to replenish their energy.  This makes them difficult
@@ -8,7 +10,7 @@
  */
 public class Grok
 {
-    private static final int DEFAULT_POWER_LEVEL=50;
+    private static final int DEFAULT_POWER_LEVEL= 50;
     private static final int MAX_POWER_LEVEL = 100;
 
     // instance variables
@@ -30,8 +32,10 @@ public class Grok
      */
     public Grok(int powerLevel)
     {
-        setPowerLevel(powerLevel);
+        setPowerLevel(Math.min(powerLevel, 100));
+        isDead();
     }
+
 
     // accessor methods
 
@@ -46,7 +50,8 @@ public class Grok
 
     public boolean isDead()
     {
-        // TODO: replace this line with your code here
+        isAlive = powerLevel > 0;
+        return !isAlive;
     }
 
     // mutator methods
@@ -57,7 +62,9 @@ public class Grok
      */
     public void setPowerLevel(int powerLevel)
     {
-        this.powerLevel = powerLevel;
+
+        if (isAlive || powerLevel <= MAX_POWER_LEVEL)
+            this.powerLevel = powerLevel;
     }
 
     /*
@@ -66,19 +73,20 @@ public class Grok
      * @param pill The PowerPill that the this Grok.  The power
      * of the pill is added to the power level of this Grok.
      */
-    public void takePowerPill(PowerPill pill)
-    {
-        setPowerLevel(powerLevel + pill.getPower());
+    public void takePowerPill(PowerPill pill) {
+        if (powerLevel > 0) {
+            setPowerLevel(Math.min(powerLevel + pill.getPower(), 100));
+        }
     }
-
     /*
      * Invoked when this Grok takes a hit.  The power level of
      * this Grok is reduced by 5.
      */
     public void tookHit()
     {
-        setPowerLevel(powerLevel - 5);
-    }
+        setPowerLevel(Math.max(powerLevel - 5, 0));
+        isDead();
+;    }
 
     //================== Do Not Touch Code Below this line =============================
     public String toString(){
